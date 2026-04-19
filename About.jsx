886d398190bd2473e7f7aa248de2253934d7fb72
Ranaganath B M <ranganathbmranganathbm1@gmail.com>
2026-04-19 21:59:39 +0530

    About.jsx

---
+++ About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Welcome to Paradise Nursery! We provide beautiful indoor and outdoor plants
+        to make your home greener and healthier.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
